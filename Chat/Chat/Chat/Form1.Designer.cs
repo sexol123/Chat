5147163cba97d877c25dd965df29edecd52d6db9
serg <sergeimaleev@gmail.com>
2017-09-03 11:01:21 +0300

    Simple Chat Lan

---
+++ Chat/Chat/Chat/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Chat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBoxMessage = new System.Windows.Forms.RichTextBox();
+            this.richTextBoxHistory = new System.Windows.Forms.RichTextBox();
+            this.textBoxNickname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxIP = new System.Windows.Forms.TextBox();
+            this.textBoxPort = new System.Windows.Forms.TextBox();
+            this.textBoxLocalPort = new System.Windows.Forms.TextBox();
+            this.buttonCloseCon = new System.Windows.Forms.Button();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBoxMessage
+            // 
+            this.richTextBoxMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxMessage.AutoWordSelection = true;
+            this.richTextBoxMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBoxMessage.Location = new System.Drawing.Point(0, 204);
+            this.richTextBoxMessage.Name = "richTextBoxMessage";
+            this.richTextBoxMessage.ShortcutsEnabled = false;
+            this.richTextBoxMessage.Size = new System.Drawing.Size(291, 64);
+            this.richTextBoxMessage.TabIndex = 1;
+            this.richTextBoxMessage.Text = "";
+            // 
+            // richTextBoxHistory
+            // 
+            this.richTextBoxHistory.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBoxHistory.BackColor = System.Drawing.Color.Cyan;
+            this.richTextBoxHistory.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBoxHistory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBoxHistory.Location = new System.Drawing.Point(0, 119);
+            this.richTextBoxHistory.Name = "richTextBoxHistory";
+            this.richTextBoxHistory.ReadOnly = true;
+            this.richTextBoxHistory.Size = new System.Drawing.Size(384, 89);
+            this.richTextBoxHistory.TabIndex = 2;
+            this.richTextBoxHistory.Text = "";
+            // 
+            // textBoxNickname
+            // 
+            this.textBoxNickname.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxNickname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxNickname.Location = new System.Drawing.Point(75, 12);
+            this.textBoxNickname.Name = "textBoxNickname";
+            this.textBoxNickname.Size = new System.Drawing.Size(218, 19);
+            this.textBoxNickname.TabIndex = 3;
+            this.textBoxNickname.Text = "User";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.label1.Location = new System.Drawing.Point(13, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "IP";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Port";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "LocalPort";
+            // 
+            // textBoxIP
+            // 
+            this.textBoxIP.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxIP.Location = new System.Drawing.Point(75, 35);
+            this.textBoxIP.Name = "textBoxIP";
+            this.textBoxIP.Size = new System.Drawing.Size(218, 19);
+            this.textBoxIP.TabIndex = 8;
+            this.textBoxIP.Text = "127.0.0.1";
+            // 
+            // textBoxPort
+            // 
+            this.textBoxPort.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxPort.Location = new System.Drawing.Point(75, 57);
+            this.textBoxPort.Name = "textBoxPort";
+            this.textBoxPort.Size = new System.Drawing.Size(218, 19);
+            this.textBoxPort.TabIndex = 9;
+            this.textBoxPort.Text = "5001";
+            // 
+            // textBoxLocalPort
+            // 
+            this.textBoxLocalPort.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxLocalPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxLocalPort.Location = new System.Drawing.Point(75, 83);
+            this.textBoxLocalPort.Name = "textBoxLocalPort";
+            this.textBoxLocalPort.Size = new System.Drawing.Size(218, 19);
+            this.textBoxLocalPort.TabIndex = 10;
+            this.textBoxLocalPort.Text = "5002";
+            // 
+            // buttonCloseCon
+            // 
+            this.buttonCloseCon.Enabled = false;
+            this.buttonCloseCon.Location = new System.Drawing.Point(300, 79);
+            this.buttonCloseCon.Name = "buttonCloseCon";
+            this.buttonCloseCon.Size = new System.Drawing.Size(75, 23);
+            this.buttonCloseCon.TabIndex = 12;
+            this.buttonCloseCon.Text = "Exit";
+            this.buttonCloseCon.UseVisualStyleBackColor = true;
+            this.buttonCloseCon.Visible = false;
+            this.buttonCloseCon.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.BackgroundImage = global::Chat.Properties.Resources.logo;
+            this.buttonConnect.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonConnect.Cursor = System.Windows.Forms.Cursors.Default;
+            this.buttonConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonConnect.ForeColor = System.Drawing.Color.Gold;
+            this.buttonConnect.Location = new System.Drawing.Point(300, 12);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 61);
+            this.buttonConnect.TabIndex = 11;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSend.BackgroundImage = global::Chat.Properties.Resources.orc;
+            this.buttonSend.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonSend.Enabled = false;
+            this.buttonSend.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonSend.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonSend.ForeColor = System.Drawing.Color.Gold;
+            this.buttonSend.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.buttonSend.Location = new System.Drawing.Point(285, 204);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(99, 64);
+            this.buttonSend.TabIndex = 0;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkCyan;
+            this.ClientSize = new System.Drawing.Size(384, 261);
+            this.Controls.Add(this.buttonCloseCon);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.textBoxLocalPort);
+            this.Controls.Add(this.textBoxPort);
+            this.Controls.Add(this.textBoxIP);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxNickname);
+            this.Controls.Add(this.richTextBoxHistory);
+            this.Controls.Add(this.richTextBoxMessage);
+            this.Controls.Add(this.buttonSend);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chat by Maleev Sergei";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.RichTextBox richTextBoxMessage;
+        private System.Windows.Forms.TextBox textBoxNickname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxIP;
+        private System.Windows.Forms.TextBox textBoxPort;
+        private System.Windows.Forms.TextBox textBoxLocalPort;
+        private System.Windows.Forms.Button buttonConnect;
+        public System.Windows.Forms.RichTextBox richTextBoxHistory;
+        private System.Windows.Forms.Button buttonCloseCon;
+    }
+}
+
---
+++ Chat/Chat/Chat/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chat
+{
+    public partial class Form1 : Form
+    {
+        private static IPAddress remoteIPAddress;
+        private static int remotePort;
+        private static int localPort;
+        private static string name;
+        UdpClient sender;
+        Thread tListener = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+           
+            try
+            {
+                name = textBoxNickname.Text;
+                remoteIPAddress = IPAddress.Parse(textBoxIP.Text);
+                localPort = Convert.ToInt16(textBoxLocalPort.Text);
+                remotePort = Convert.ToInt16(textBoxPort.Text);
+                tListener = new Thread(new ThreadStart(Receiver));
+                tListener.IsBackground = true;
+                tListener.Start();
+                buttonSend.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void Receiver()
+        {
+            UdpClient receivingUdpClient = null;
+            IPEndPoint RemoteIpEndPoint = null;
+           
+            try
+            {
+                receivingUdpClient = new UdpClient(localPort);
+               this.Invoke(new Action(() =>
+                {
+                    richTextBoxHistory.Text +=
+                  $"Connected {DateTime.Now}\n-----------*******Общий чат*******-----------";
+                }));
+               
+
+                while (true)
+                {
+                    // Ожидание дейтаграммы
+                    byte[] receiveBytes = receivingUdpClient.Receive(
+                       ref RemoteIpEndPoint);
+
+                    // Преобразуем и отображаем данные
+                    string returnData = Encoding.UTF8.GetString(receiveBytes);
+                    
+                   this.Invoke(new Action(() =>
+                    {
+                        richTextBoxHistory.Text +=returnData.ToString();
+                    }));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+        private static string Send(string datagram)
+        {
+            // Создаем UdpClient
+            UdpClient sender = null;
+            if (sender == null)
+                sender = new UdpClient();
+
+            // Создаем endPoint по информации об удаленном хосте
+            IPEndPoint endPoint = null;
+            if(endPoint == null)
+                endPoint = new IPEndPoint(remoteIPAddress, remotePort);
+
+            try
+            {
+                // Преобразуем данные в массив байтов
+                byte[] bytes = Encoding.UTF8.GetBytes(datagram);
+
+                // Отправляем данные
+                sender.Send(bytes, bytes.Length, endPoint);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                // Закрыть соединение
+                sender.Client.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            return datagram;
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            
+            if (richTextBoxMessage.Text == String.Empty)
+            {
+                return;
+            }
+            this.richTextBoxHistory.Text += Send("\n" + DateTime.Now.ToLongTimeString() + " " +
+                   name + " " + richTextBoxMessage.Text);
+            this.richTextBoxMessage.Text = String.Empty;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BeginInvoke(new Action(() => {
+                tListener.Suspend();
+            }));
+                
+        }
+    }
+}
